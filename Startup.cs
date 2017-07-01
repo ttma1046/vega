c4dbf9902cd5f60d78dbe4397175f0de1765be93
Chandler Fang <chandlerfang@gmail.com>
2017-07-01 19:00:33 +1000

    20.Using Configuration.

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+
+using Microsoft.EntityFrameworkCore;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using vega.Persistence;
 
 namespace WebApplicationBasic
 {
@@ -28,6 +32,8 @@ namespace WebApplicationBasic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"])); // Configuration.GetConnectionString("")
+
             // Add framework services.
             services.AddMvc();
         }
