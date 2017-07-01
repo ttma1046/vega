5b5ee1e0c9a83742e227413e90c5d971a3042832
Chandler Fang <chandlerfang@gmail.com>
2017-07-01 21:50:03 +1000

    28. AutoMapper.

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+using AutoMapper;
+
 using vega.Persistence;
 
 namespace WebApplicationBasic
@@ -32,6 +35,8 @@ namespace WebApplicationBasic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
+
             services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"])); // Configuration.GetConnectionString("")
 
             // Add framework services.
